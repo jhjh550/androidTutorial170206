73cfad8473d7673737b118cde52913cb1a07881c
Your N             ame <you@e             xample.com>
2017-02-07 11:39:58 +0900

    - listview click

--- 05_listview/app/src/main/java/com/example/a/a05_listview/MainActivity.java
+++ 05_listview/app/src/main/java/com/example/a/a05_listview/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.a.a05_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +25,11 @@ public class MainActivity extends AppCompatActivity {
                 this, android.R.layout.simple_list_item_1, list
         );
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "value : "+list[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
