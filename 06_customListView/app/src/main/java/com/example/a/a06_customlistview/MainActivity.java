97881bc7186d702c130d76b0c17a0ce293019a3e
Your N             ame <you@e             xample.com>
2017-02-07 12:20:43 +0900

    - custom adapter

--- 06_customListView/app/src/main/java/com/example/a/a06_customlistview/MainActivity.java
+++ 06_customListView/app/src/main/java/com/example/a/a06_customlistview/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.a.a06_customlistview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -11,6 +17,41 @@ public class MainActivity extends AppCompatActivity {
         String title;
         String desc;
         int imgIcon;
+    }
+
+    class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null){
+                convertView = getLayoutInflater().inflate(R.layout.item_view, null);
+            }
+            TextView titleTextView = (TextView) convertView.findViewById(R.id.titleTextView);
+            TextView descTextView = (TextView) convertView.findViewById(R.id.descTextView);
+            ImageView imgIcon = (ImageView) convertView.findViewById(R.id.itemIcon);
+
+            MyData data = list.get(position);
+            titleTextView.setText(data.title);
+            descTextView.setText(data.desc);
+            imgIcon.setImageResource(data.imgIcon);
+
+            return convertView;
+        }
     }
 
     ArrayList<MyData> list = new ArrayList<MyData>();
@@ -26,5 +67,9 @@ public class MainActivity extends AppCompatActivity {
             data.imgIcon = R.mipmap.ic_launcher;
             list.add(data);
         }
+
+        ListView listView = (ListView) findViewById(R.id.myListView);
+        MyAdapter adapter = new MyAdapter();
+        listView.setAdapter(adapter);
     }
 }
