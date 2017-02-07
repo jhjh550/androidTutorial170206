a161e29b479b0cdb1ffccffdb14988fb6743017d
Your N             ame <you@e             xample.com>
2017-02-07 10:19:24 +0900

    - action item

--- 04_menu/app/src/main/java/com/example/a/a04_menu/MainActivity.java
+++ 04_menu/app/src/main/java/com/example/a/a04_menu/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.a.a04_menu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_test1:
+                Toast.makeText(this, "menu1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_test2:
+                Toast.makeText(this, "menu2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_test3:
+                Toast.makeText(this, "menu3", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
