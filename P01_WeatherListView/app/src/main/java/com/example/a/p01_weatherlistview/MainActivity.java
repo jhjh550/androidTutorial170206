1f84e14283af5f145d844b8ea4fb05f73bcea45f
Your N             ame <you@e             xample.com>
2017-02-07 17:02:49 +0900

    - dateformat

--- P01_WeatherListView/app/src/main/java/com/example/a/p01_weatherlistview/MainActivity.java
+++ P01_WeatherListView/app/src/main/java/com/example/a/p01_weatherlistview/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.a.p01_weatherlistview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +19,41 @@ public class MainActivity extends AppCompatActivity {
         float temperature;
         int day;
         int hour;
+    }
+    class WeatherAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return weatherList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return weatherList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null){
+                convertView = getLayoutInflater().inflate(R.layout.item_view, null);
+            }
+            TextView textView1 = (TextView)convertView.findViewById(R.id.weatherInfo1);
+            TextView textView2 = (TextView)convertView.findViewById(R.id.weatherInfo2);
+            WeatherData data = weatherList.get(position);
+            String info1 = "날씨 : "+data.weather + ", 온도 : "+ data.temperature;
+            Date date = new Date();
+            date.setDate(data.hour);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일  HH시");
+            String info2 = sdf.format(date);
+            textView1.setText(info1);
+            textView2.setText(info2);
+            return convertView;
+        }
     }
     ArrayList<WeatherData> weatherList = new ArrayList<>();
     @Override
@@ -40,5 +82,9 @@ public class MainActivity extends AppCompatActivity {
             data.hour = (i*3)%24;
             weatherList.add(data);
         }
+
+        ListView listView = (ListView) findViewById(R.id.myListView);
+        WeatherAdapter adapter = new WeatherAdapter();
+        listView.setAdapter(adapter);
     }
 }
