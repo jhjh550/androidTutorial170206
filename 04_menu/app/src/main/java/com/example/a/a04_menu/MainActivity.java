6a1b4ca2a32a2a7ab9359985edd89cafdb8c7f1f
Your N             ame <you@e             xample.com>
2017-02-07 10:01:19 +0900

    - menu

---
+++ 04_menu/app/src/main/java/com/example/a/a04_menu/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.a.a04_menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
