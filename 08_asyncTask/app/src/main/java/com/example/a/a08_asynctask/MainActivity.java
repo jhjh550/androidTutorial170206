81fbd0a27f51b2fc711339ba12eddb5905e43a89
Your N             ame <you@e             xample.com>
2017-02-07 14:59:00 +0900

    async Task

--- 08_asyncTask/app/src/main/java/com/example/a/a08_asynctask/MainActivity.java
+++ 08_asyncTask/app/src/main/java/com/example/a/a08_asynctask/MainActivity.java
@@ -4,15 +4,35 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView myTextView;
     class MyAsyncTask extends AsyncTask<Integer, Float, String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            myTextView.setText(s);
+        }
+
+        @Override
+        protected void onProgressUpdate(Float... values) {
+            super.onProgressUpdate(values);
+            myTextView.setText("count : "+values[0]);
+        }
+
         @Override
         protected String doInBackground(Integer... params) {
             int i = params[0];
             for(; i<100; i++){
                 Log.d("AsyncTask", "count : "+i);
+                publishProgress((float)i);
+//                myTextView.setText("count : "+i);
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
@@ -26,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myTextView = (TextView) findViewById(R.id.myTextView);
 
         MyAsyncTask task = new MyAsyncTask();
         task.execute(30);
