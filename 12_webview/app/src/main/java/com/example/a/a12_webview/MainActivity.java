8ef8377163e26327ee22fcf2c884e84df76e6871
Your N             ame <you@e             xample.com>
2017-02-08 14:50:48 +0900

    MyWebviewClient

--- 12_webview/app/src/main/java/com/example/a/a12_webview/MainActivity.java
+++ 12_webview/app/src/main/java/com/example/a/a12_webview/MainActivity.java
@@ -1,19 +1,38 @@
 package com.example.a.a12_webview;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
     ProgressDialog dlg;
     WebView webView;
     class MyWebViewClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if(url.startsWith("intent://")){
+
+                try {
+                    Intent intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
+
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+                return true;
+            }
+            return false;
+        }
+
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             super.onPageStarted(view, url, favicon);
@@ -34,8 +53,21 @@ public class MainActivity extends AppCompatActivity {
         dlg = new ProgressDialog(this);
         webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.setHorizontalScrollBarEnabled(false); //가로 스크롤
+        webView.setVerticalScrollBarEnabled(false);   //세로 스크롤
         webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl("http://www.daum.net");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            finish();
+        }
     }
 
     public void onBtnClick(View v){
