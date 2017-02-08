ecc4794eee358cbd6e560adf9a89f8eaf6799948
Your N             ame <you@e             xample.com>
2017-02-08 14:04:48 +0900

    MyWebviewClient

--- 12_webview/app/src/main/java/com/example/a/a12_webview/MainActivity.java
+++ 12_webview/app/src/main/java/com/example/a/a12_webview/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.a.a12_webview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressDialog dlg;
+    WebView webView;
+    class MyWebViewClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            dlg.show();
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            dlg.dismiss();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dlg = new ProgressDialog(this);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.loadUrl("http://www.daum.net");
+    }
+
+    public void onBtnClick(View v){
+        EditText editURL = (EditText) findViewById(R.id.editURL);
+        String str = editURL.getText().toString();
+        webView.loadUrl(str);
     }
 }
