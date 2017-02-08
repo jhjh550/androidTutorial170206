0576c438e27a1123178e2e188f322dd2d3b9061c
Your N             ame <you@e             xample.com>
2017-02-08 11:11:40 +0900

    sqlite open helper

---
+++ 11_sqlite/app/src/main/java/com/example/a/a11_sqlite/TestSQLiteOpenHelper.java
@@ -0,0 +1,29 @@
+package com.example.a.a11_sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by a on 2017-02-08.
+ */
+
+public class TestSQLiteOpenHelper extends SQLiteOpenHelper {
+    public TestSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String strSQL = "CREATE TABLE student (id INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                "name TEXT, age INTEGER, address TEXT);";
+        db.execSQL(strSQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String strSQL = "DROP TABLE IF EXISTS student";
+        db.execSQL(strSQL);
+        onCreate(db);
+    }
+}
