4445c35b4f58c210b8982f944b3b70df0b1c56b7
Your N             ame <you@e             xample.com>
2017-02-09 14:19:20 +0900

    camera

---
+++ 18_service2/app/src/main/java/com/example/a/a18_service2/MyIntentService2.java
@@ -0,0 +1,26 @@
+package com.example.a.a18_service2;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions and extra parameters.
+ */
+public class MyIntentService2 extends IntentService {
+    public MyIntentService2() {
+        super("MyIntentService2");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+
+        }
+    }
+
+
+}
