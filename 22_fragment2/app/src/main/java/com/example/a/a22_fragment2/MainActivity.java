4436f15cc69f42bde7f0d1d3916f563bcf5a4dd1
Your N             ame <you@e             xample.com>
2017-02-10 13:59:08 +0900

    fragment 2

--- 22_fragment2/app/src/main/java/com/example/a/a22_fragment2/MainActivity.java
+++ 22_fragment2/app/src/main/java/com/example/a/a22_fragment2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.a.a22_fragment2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onBtnClick(View v){
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.frame);
+        switch (v.getId()){
+            case R.id.btnAdd:
+                if(fragment == null){
+                    fragment = new BlankFragment();
+                    FragmentTransaction tr = fm.beginTransaction();
+                    tr.add(R.id.frame, fragment, "counter");
+                    tr.commit();
+                }
+                break;
+            case R.id.btnRemove:
+                break;
+            case R.id.btnReplace:
+                break;
+            case R.id.btnHide:
+                break;
+        }
+    }
 }
+
+
+
+
+
+
+
+
