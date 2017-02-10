3b8c4ce27963334d11c30a2f8ab89b1d0f55ebff
Your N             ame <you@e             xample.com>
2017-02-10 12:20:22 +0900

    fragment 2

---
+++ 22_fragment2/app/src/main/java/com/example/a/a22_fragment2/BlankFragment.java
@@ -0,0 +1,44 @@
+package com.example.a.a22_fragment2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        final TextView textView = (TextView) view.findViewById(R.id.counterTextView);
+        Button btn = (Button)view.findViewById(R.id.btnIncrease);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = textView.getText().toString();
+                int value = Integer.parseInt(str);
+                value += 1;
+                textView.setText(""+value);
+            }
+        });
+
+        return view;
+    }
+
+}
